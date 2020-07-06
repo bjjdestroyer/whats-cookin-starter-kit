fa7ff8389a8bded34bdc82710beaef79d172a591
Ben McClung <aurumvalian@gmail.com>
2020-07-06 17:20:01 -0600

    Add and fill constructor for Recipe class

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,5 +1,12 @@
 class Recipe {
-  
+  constructor(id, image, ingredients, instructions, name, tags) {
+    this.id = id;
+    this.image = image;
+    this,ingredients = ingredients;
+    this.instructions = instructions;
+    this.name = name;
+    this.tags = tags;
+  }
 }
 
 module.exports = Recipe;
