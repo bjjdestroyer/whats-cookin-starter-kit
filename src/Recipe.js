ebfc7a0488cf67d4f12df7e9e532c7aee2d37225
Ben McClung <aurumvalian@gmail.com>
2020-07-06 17:36:52 -0600

    Start getIngredientCost method in Recipe.js

--- src/Recipe.js
+++ src/Recipe.js
@@ -7,6 +7,12 @@ class Recipe {
     this.name = name;
     this.tags = tags;
   }
+
+  getIngredientCost() {
+    this.ingredients.reduce(ingredient, totalPrice, function() {
+      totalPrice + (ingredient.quantity.amount * /* ingredientPrice method, where ingredientPrice takes in an id and returns the estimatedCostInCents from that id's object*/)
+    }, 0)
+  }
 }
 
 module.exports = Recipe;
