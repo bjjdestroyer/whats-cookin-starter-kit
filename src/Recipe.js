dab109c6ee2defac5ab870ea84fe146b1a29d013
Ben McClung <aurumvalian@gmail.com>
2020-07-06 18:03:44 -0600

    Change , to . in Recipe.js

--- src/Recipe.js
+++ src/Recipe.js
@@ -2,7 +2,7 @@ class Recipe {
   constructor(id, image, ingredients, instructions, name, tags) {
     this.id = id;
     this.image = image;
-    this,ingredients = ingredients;
+    this.ingredients = ingredients;
     this.instructions = instructions;
     this.name = name;
     this.tags = tags;
