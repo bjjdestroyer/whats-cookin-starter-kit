f8db202c99a2add7f47738d65df9ec7fb2ae8f1e
Taryn Martin <taryn.martin919@gmail.com>
2020-07-07 15:17:23 -0600

    Finish test cases and remove test cases for Recipes

--- test/recipe-test.js
+++ test/recipe-test.js
@@ -7,9 +7,11 @@ const recipes = requires('../data/recipes');
 
 describe('Recipe', function () {
   let recipe;
+
   beforeEach(function () {
     recipe = new Recipe();
-  })
+  });
+
   it('should be a function', function () {
     expect(Recipe).to.be.a('function');
   });
@@ -17,67 +19,6 @@ describe('Recipe', function () {
   it('should be an instance of Recipe', function () {
     expect(recipe).to.be.an.instanceOf(Recipe);
   });
-
-  it('should have an array of recipes', function () {
-    expect(recipe.allRecipes).to.deep.equal(recipes);
-  });
-
-  it('should be able to filter recipes by tag', function () {
-    const filteredByTag = recipe.filterByTag("sauce");
-    //return id's at this point?
-    expect(filteredByTag).to.deep.equal(recipes[2].id);
-  });  
-
-  it('should be able to filter and return multiple recipes by tag', function () {
-    const filteredByTag = recipe.filterByTag("salad");
-    const recipeIds = [793584, 999044, 576906, 799732]
-    // check out array order
-    //return id's at this point?
-    expect(filteredByTag).to.deep.equal(recipeIds);
-  });  
-
-  it('should be able to filter recipes with no tag', function () {
-    const filteredByTag = recipe.filterByTag();
-    const recipeID = [562334];
-    //return id's at this point?
-    // or have the return equal index of recipes data?
-    expect(filteredByTag).to.deep.equal(recipeID);
-  }); 
-
-  it('should be able to find recipes by ingredient', function () {
-    const filteredByIngredient = recipe.filterByIngredient("bicarbonate of soda");
-    const recipeIds = [595736, 623855, 611858, 583738];
-    //return id's at this point?
-    // or have the return equal index of recipes data?
-
-    expect(filteredByIngredient).to.deep.equal(recipeIds);
-  });  
-
-  it('should be able to find all recipes that have the same word in it', function () {
-    const filteredByIngredient = recipe.filterByIngredient("soda");
-    const recipeIds = [595736, 623855, 611858, 583738, 602311];
-    //return id's at this point?
-    // or have the return equal index of recipes data?
-
-    expect(filteredByIngredient).to.deep.equal(recipeIds);
-  });  
-
-  it('should be able to return a message if no recipe has that ingredient', function () {
-    let noIngredient = recipe.filteredByIngredient('rhubarb');
-
-    expect(noIngredient).to.equal('No recipe has that ingredient');
-  });  
-
-  it('should be able to return a message if it\'s an invalid ingredient', function () {
-    let noIngredient = recipe.filteredByIngredient('12');
-    expect(noIngredient).to.equal('Please enter a valid ingredient');
-
-    let noIngredient = recipe.filteredByIngredient('*');
-    expect(noIngredient).to.equal('Please enter a valid ingredient');
-
-    let noIngredient = recipe.filteredByIngredient('  ');
-    expect(noIngredient).to.equal('Please enter a valid ingredient');
-  }); 
 
   it('should be able to calculate recipe cost', function () {
     const recipeCost = recipe.getTotalIngredientCost(595736);
