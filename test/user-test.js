d1b0f2a33ab8ce0e413361b842d970bfa0bdbcbf
Ben McClung <aurumvalian@gmail.com>
2020-07-07 15:38:56 -0600

    Add assertion import andbeforeEach function to user-test

---
+++ test/user-test.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const User = require('./src/User');
+
+describe('User', function() {
+  let user;
+
+  beforeEach(function() {
+    user = new User;
+  })
+
+  
+})
