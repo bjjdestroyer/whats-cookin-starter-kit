cf020ad9340abecb357104669ee59b534a2e1a95
Ben McClung <aurumvalian@gmail.com>
2020-07-07 17:40:36 -0600

    Add property tests in user-test and switch parameters in reduce function in Recipe

--- test/user-test.js
+++ test/user-test.js
@@ -2,12 +2,13 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const User = require('../src/User');
+const users = require('../data/users');
 
 describe('User', function() {
   let user;
 
   beforeEach(function() {
-    user = new User();
+    user = new User(users[0]);
   })
 
   it('should be a function', function() {
@@ -19,6 +20,17 @@ describe('User', function() {
   })
 
   it('should have a name', function() {
-
+    expect(user.name).to.equal("Saige O'Kon");
   })
+
+  it('should have a user ID', function() {
+    expect(user.id).to.equal(1);
+  })
+
+  it('should have a pantry', function() {
+    console.log(user.pantry);
+    expect(user.pantry).to.be.an("array");
+  })
+
+  
 })
