f998e2699fe0b206bdff7db2770eb2daee0b223d
Ben McClung <aurumvalian@gmail.com>
2020-07-07 14:27:59 -0600

    Add filterRecipesByTag method to Recipes class

--- src/Recipes.js
+++ src/Recipes.js
@@ -1,7 +1,11 @@
 const Recipe = require('../Recipe');
 
 class Recipes {
-  constructor() {
-    
+  constructor(recipes) {
+    this.recipes = recipes;
+  }
+
+  filterRecipesByTag(tag) {
+    return this.recipes.filter(recipe => recipe.tags.includes(tag));
   }
 }
