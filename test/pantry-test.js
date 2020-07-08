9585e7828e8038d95a5e50b4f66532bbddcf2517
Ben McClung <aurumvalian@gmail.com>
2020-07-08 13:59:55 -0600

    Add tests for pantry methods

--- test/pantry-test.js
+++ test/pantry-test.js
@@ -3,6 +3,8 @@ const expect = chai.expect;
 
 const Pantry = require('../src/Pantry');
 const users = require('../data/users');
+const recipes = require('../data/recipes');
+
 
 describe('Pantry', function() {
   let pantry;
@@ -20,6 +22,23 @@ describe('Pantry', function() {
   })
 
   it('should contain an array of ingredients', function() {
-    expect(pantry.contents[3].ingredient).to,equal(1082047);
+    expect(pantry.contents[3].ingredient).to.equal(1082047);
   })
+
+  it('should be able to determine if it has enough ingredients for a given recipe', function() {
+    let testRecipe = recipes[0];
+    let response = pantry.canMakeRecipe(testRecipe);
+    expect(response).to.equal("You do not have sufficient ingredients for this recipe.");
+  })
+
+  it('should return how much of each ingredient still needed', function() {
+    let testRecipe = recipes[0];
+    let missingIngredients = pantry.findMissingIngredients(testRecipe);
+    expect(missingIngredients).to.equal({"19206": 1, "19334": 0.5, "1012047": 24, 10019903: 2});
+  })
+
+  // it('should be able to remove ingredients from a cooked meal', function() {
+  //   let testRecipe = recipes[0];
+    
+  // })
 })
