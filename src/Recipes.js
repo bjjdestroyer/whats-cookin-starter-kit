2025436fad6eba1b3d85d709d6a2bbcf80aecf4f
Taryn Martin <taryn.martin919@gmail.com>
2020-07-09 14:54:35 -0600

    Refactor filter by ingredient method #37

--- src/Recipes.js
+++ src/Recipes.js
@@ -1,8 +1,10 @@
+const fullIngredientsList = require('../data/ingredients');
+const allRecipes = require('../data/recipes');
 const Recipe = require('./Recipe');
 
 class Recipes {
-  constructor(recipes) {
-    this.recipes = recipes;
+  constructor() {
+    this.recipes = allRecipes;
   }
 
   filterByTag(tag) {
@@ -17,7 +19,35 @@ class Recipes {
   }
 
   filterByIngredient(ingredientName) {
-    return this.recipes.filter(recipe => recipe.getIngredientNameList().includes(ingredientName));
+    // const ingredientList = this.recipes.filter(recipe => {
+    //   return recipe.getIngredientNameList();
+    // }); // this will return names of all ingredients in all recipes
+
+    // return this.recipes.filter( recipe => {
+    //   if (ingredientList.includes(ingredientName)) {
+    //     return recipe;
+    //   }
+    // });
+
+    const ingredientNeeded = fullIngredientsList.find( ingredient => {
+      return ingredient.name === ingredientName;
+    });
+
+    let recipeArray = []
+
+    const recipes = this.recipes.forEach( recipe => {
+      recipe.ingredients.forEach( ingredient => {
+        if (ingredient.id === ingredientNeeded.id) {
+          recipeArray.push(recipe);
+        };
+      });
+    });
+
+    return recipeArray;
+    // we want to return all recipes that contain ingredient being searched for
+    // User inputs item they're looking for
+    // Names aren't in recipes, need to look through ingredients list for names
+    // Once we have names, return recipes they're in
   }
 }
 
