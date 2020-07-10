9443e9c67662cfe130086146e94e7c3fdf510e61
Ben McClung <aurumvalian@gmail.com>
2020-07-10 16:50:27 -0600

    Add instantiation functions to scripts.js

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,17 @@
-console.log('Hello world');
+const user = require('./User');
+
+
+
+function instantiateRecipes() {
+  return new Recipes();
+}
+
+function instantiateEachRecipe(recipes) {
+  recipes.recipes = recipes.recipes.map(recipe => {
+    return new Recipe(recipe);
+  });
+}
+
+function instantiateUser(user) {
+  return new User(user);
+}
