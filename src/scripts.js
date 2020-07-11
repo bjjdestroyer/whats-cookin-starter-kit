fa3e449fa7afa072dcb66f71cced485ab111f0d1
Ben McClung <aurumvalian@gmail.com>
2020-07-11 14:59:05 -0600

    Add populateList method to scripts and getPantryIngredients method to user

--- src/scripts.js
+++ src/scripts.js
@@ -2,6 +2,7 @@ const userName = document.querySelector('.user-name');
 const pantryModal = document.querySelector('.pantry-modal');
 const pantryButton = document.querySelector('.pantry');
 const closeButton = document.querySelector('.close-button');
+const pantryList = document.querySelector('.pantry-list');
 
 let recipes;
 let user;
@@ -30,12 +31,17 @@ function instantiateUser(user) {
   return new User(user);
 }
 
+function populateList() {
+  pantryList.innerText = user.getPantryIngredients();
+}
+
 function instantiateWebsiteOnLoad() {
   recipes = instantiateRecipes();
   instantiateEachRecipe(recipes);
   let randomUserIndex = getRandomIndex(usersData);
   user = instantiateUser(usersData[randomUserIndex]);
   populateUser(user);
+  populateList();
 }
 
 function getRandomIndex(array) {
