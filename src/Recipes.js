b227c3bbce2ef1246a7ea6e9920b0f5b09f0a064
Taryn Martin <taryn.martin919@gmail.com>
2020-07-11 10:20:42 -0600

    Split filter by ingredient method #37

--- src/Recipes.js
+++ src/Recipes.js
@@ -1,6 +1,6 @@
-// const fullIngredientsList = require('../data/ingredients');
-// const allRecipes = require('../data/recipes');
-// const Recipe = require('./Recipe');
+const fullIngredientsList = require('../data/ingredients');
+const recipeData = require('../data/recipes');
+const Recipe = require('./Recipe');
 
 class Recipes {
   constructor() {
@@ -16,17 +16,22 @@ class Recipes {
         return noTag;
       }
     });
-  }
+  };
 
-  filterByIngredient(ingredientName) {
-    const ingredientNeeded = fullIngredientsList.filter( ingredient => {
-      let newIngredient = ingredient.name;
-      if (newIngredient === undefined) {
+  getAllIngredients(ingredientName) {
+    const ingredientNeeded = fullIngredientsList.filter(ingredient => {
+      if (ingredient.name === undefined) {
         return;
-      } else if (newIngredient.includes(ingredientName)) {
+      } else if (ingredient.name.includes(ingredientName)) {
         return ingredient;
       }
     });
+
+    return ingredientNeeded;
+  };
+
+  filterByIngredient(ingredientName) {
+    const ingredientNeeded = this.getAllIngredients(ingredientName);
 
     let recipeArray = []
 
