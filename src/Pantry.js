ab07aeed04867f8bafb4ac5b452aeeb3285ef231
Taryn Martin <taryn.martin919@gmail.com>
2020-07-11 10:37:00 -0600

    Refactor based on lint feedback #86

--- src/Pantry.js
+++ src/Pantry.js
@@ -11,15 +11,17 @@ class Pantry {
       neededIngredients[ingredient.id] = ingredient.quantity.amount;
     });
     return neededIngredients;
-  };
+  }
 
   addingNeededIngredients(neededIngredients) {
+    const keysNeeded = Object.keys(neededIngredients);
+
     return this.contents.forEach(ingredient => {
-      if (Object.keys(neededIngredients).includes(ingredient.ingredient.toString())) {
+      if (keysNeeded.includes(ingredient.ingredient.toString())) {
         neededIngredients[ingredient.ingredient] -= ingredient.amount;
       }
     });
-  };
+  }
 
   canMakeRecipe(recipe) {
     let neededIngredients = this.findingNeededIngredients(recipe);
@@ -32,7 +34,7 @@ class Pantry {
     } else {
       return "You have enough ingredients for this recipe!"
     }
-  };
+  }
 
   findMissingIngredients(recipe) {
     let ingredientsNeeded = this.findingNeededIngredients(recipe);
@@ -46,9 +48,9 @@ class Pantry {
     });
 
     return ingredientsToBuy;
-  };
+  }
 }
 
-if(typeof(module) !== 'undefined') {
+if (typeof(module) !== 'undefined') {
   module.exports = Pantry;
 }
